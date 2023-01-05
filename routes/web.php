93921673119263167610fff4fd21862d965a7e0f
Yoga Aditya Nugraha <108383702+yoga-nditya@users.noreply.github.com>
2023-01-05 19:00:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,83 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+namespace App\Http\Controllers\API;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
+use Illuminate\Http\Request;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $category = Category::all();
+        $categoryResource = CategoryResource::collection($category);
+
+        return $this->sendResponse($categoryResource, "Successfull get category");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $data = new CategoryResource(Category::create($request->validated()));
+        return $this->sendResponse($data, "Successfull store");
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        $cek = Category::findOrFail($category);
+        if (!$cek) {
+            abort(404, 'Object not found');
+        }
+        $category = new CategoryResource($category);
+
+        return $this->sendResponse($category, "Successfull get category");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreCategoryRequest $request, Category $category)
+    {
+        $category->update($request->validated());
+
+        $result = new CategoryResource($category);
+
+        return $this->sendResponse($result, "Successfull updated category");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return response()->noContent();
+    }
+}
